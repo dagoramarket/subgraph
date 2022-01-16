7115cb6d2ba1dc4b91fcb865325549ff07f38e06
Gustavo Inacio <incio.gusta@gmail.com>
2022-01-16 20:21:26 -0300

    Initial commit

---
+++ src/mapping.ts
@@ -0,0 +1,71 @@
+import { BigInt } from "@graphprotocol/graph-ts"
+import {
+  ListingManager,
+  ListingCancelled,
+  ListingCreated,
+  ListingReportResult,
+  ListingReported,
+  ListingUpdated,
+  OwnershipTransferred
+} from "../generated/ListingManager/ListingManager"
+import { ExampleEntity } from "../generated/schema"
+
+export function handleListingCancelled(event: ListingCancelled): void {
+  // Entities can be loaded from the store using a string ID; this ID
+  // needs to be unique across all entities of the same type
+  let entity = ExampleEntity.load(event.transaction.from.toHex())
+
+  // Entities only exist after they have been saved to the store;
+  // `null` checks allow to create entities on demand
+  if (!entity) {
+    entity = new ExampleEntity(event.transaction.from.toHex())
+
+    // Entity fields can be set using simple assignments
+    entity.count = BigInt.fromI32(0)
+  }
+
+  // BigInt and BigDecimal math are supported
+  entity.count = entity.count + BigInt.fromI32(1)
+
+  // Entity fields can be set based on event parameters
+  entity.hash = event.params.hash
+
+  // Entities can be written to the store with `.save()`
+  entity.save()
+
+  // Note: If a handler doesn't require existing field values, it is faster
+  // _not_ to load the entity from the store. Instead, create it fresh with
+  // `new Entity(...)`, set the fields that should be updated and save the
+  // entity back to the store. Fields that were not set or unset remain
+  // unchanged, allowing for partial updates to be applied.
+
+  // It is also possible to access smart contracts from mappings. For
+  // example, the contract that has emitted the event can be connected to
+  // with:
+  //
+  // let contract = Contract.bind(event.address)
+  //
+  // The following functions can then be called on this contract to access
+  // state variables and other data:
+  //
+  // - contract.MINIMUM_STAKED_TOKEN(...)
+  // - contract.PERCENTAGE_BURN(...)
+  // - contract.cancelledListings(...)
+  // - contract.createListing(...)
+  // - contract.disputeManager(...)
+  // - contract.managerAllowed(...)
+  // - contract.owner(...)
+  // - contract.requireValidListing(...)
+  // - contract.stakeManager(...)
+  // - contract.updateListing(...)
+}
+
+export function handleListingCreated(event: ListingCreated): void {}
+
+export function handleListingReportResult(event: ListingReportResult): void {}
+
+export function handleListingReported(event: ListingReported): void {}
+
+export function handleListingUpdated(event: ListingUpdated): void {}
+
+export function handleOwnershipTransferred(event: OwnershipTransferred): void {}
